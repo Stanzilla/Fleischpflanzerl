dc6197489f46f1cab01a8d5d7a741c4fca46586b
Benjamin Staneck <staneck@gmail.com>
2016-08-05 09:04:00 +0200

    new camera max

--- Modules/CVars.lua
+++ Modules/CVars.lua
@@ -4,7 +4,7 @@ StanUICV:RegisterEvent("VARIABLES_LOADED")
 StanUICV:SetScript("OnEvent", function()
 
     SetCVar("Autointeract", 0)
-	SetCVar("cameraDistanceMaxFactor", 1.9) 		 		-- Sets the factor by which cameraDistanceMax is multiplied.
+	SetCVar("cameraDistanceMaxFactor", 2.6) 		 		-- Sets the factor by which cameraDistanceMax is multiplied.
 	SetCVar("ShowClassColorInNameplate", 1) 	 			-- Turns on class coloring in nameplates.
 	SetCVar("screenshotQuality", 10) 			 			-- Screenshot quality (0-10)
 	SetCVar("useUiScale", 0)					 			-- Turns off UI scaling
@@ -18,6 +18,7 @@ StanUICV:SetScript("OnEvent", function()
 	SetCVar("gameTip", 0)
 	SetCVar("UnitNameOwn", 1)
 	SetCVar("UnitNameGuildTitle", 0)
+	SetCVar("UnitNamePlayerGuild", 0)
 	SetCVar("UnitNamePlayerPVPTitle", 0)
 	SetCVar("nameplateMaxDistance", 50)
 	SetCVar("SkyCloudLOD", 3)
