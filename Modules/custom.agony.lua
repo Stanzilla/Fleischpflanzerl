ff2374a6730dd16cd39d9a3002b542759e618f2a
Benjamin Staneck <staneck@gmail.com>
2018-10-01 02:41:54 +0200

    add agony module

---
+++ Modules/custom.agony.lua
@@ -0,0 +1,31 @@
+-- UNTESTED
+-- some icons which are important for mythic+
+-- (they should show up on the top right of nameplates)
+local folder,ns=...
+local addon = KuiNameplates
+local core = KuiNameplatesCore
+
+local mod = addon:NewPlugin('AgonyAura',101,3)
+if not mod then return end
+
+function mod:Create(f)
+    local agony = f.handler:CreateAuraFrame({
+        max = 1,
+        size = 36,
+        squareness = 1,
+        point = {'BOTTOMRIGHT','RIGHT','LEFT'},
+        rows = 1,
+        filter = 'HARMFUL',
+        whitelist = {
+            [980] = true, -- agony
+        },
+    })
+    agony:SetFrameLevel(0)
+    agony:SetWidth(32)
+    agony:SetHeight(32)
+    agony:SetPoint('LEFT',f.bg,'LEFT',-40,0)
+    f.AgonyAura = agony
+end
+function mod:Initialise()
+    self:RegisterMessage('Create')
+end
