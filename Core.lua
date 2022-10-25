cfb46f1cb6f68157bca02bee037a50bea3789a1f
Benjamin Staneck <staneck@gmail.com>
2022-10-25 21:19:38 +0200

    Adjust classic check for wrath

--- Core.lua
+++ Core.lua
@@ -3,6 +3,10 @@
 local addonName, Fleischpflanzerl = ...
 
 function Fleischpflanzerl.IsClassic()
+    return LE_EXPANSION_WRATH_OF_THE_LICH_KING == 2
+end
+
+function Fleischpflanzerl.IsClassicEra()
     return WOW_PROJECT_ID == WOW_PROJECT_CLASSIC
 end
 
